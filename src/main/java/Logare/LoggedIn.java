565090adbda7dbc65c9cfbe327fe0bd57b971f32
User <mihaiflorianenache.91@gmail.com>
2019-06-01 00:49:24 +0300

    first thread: balise-EVC first thread.join() second thread: balise-controller
    I added graphics interface

--- src/main/java/Logare/LoggedIn.java
+++ src/main/java/Logare/LoggedIn.java
@@ -134,6 +134,13 @@ public class LoggedIn {
         int haveAccount=loggedIn.doYouHaveAccount(frame);
         if(haveAccount==1) {
             loggedIn.createCredentials(frame);
+            JOptionPane.showMessageDialog(frame,"Your account has been created");
+            String options[]=new String[]{"Yes","No"};
+            JLabel label=new JLabel();
+            label.setText("Do you want to log in ?");
+            int option=JOptionPane.showOptionDialog(frame,label,"Password", JOptionPane.NO_OPTION,JOptionPane.PLAIN_MESSAGE,null,options,options[1]);
+            if(option==0)
+                loggedIn.loggedIn(frame);
         }
         else{
             loggedIn.loggedIn(frame);
@@ -148,10 +155,64 @@ public class LoggedIn {
     }
 
     private void insertPersonalUsername(JFrame frame) throws SQLException {
-        checkUsername(frame,againUsername);
+        String username=JOptionPane.showInputDialog(frame,"Enter your username");
+        int i;
+        int numberSpace=0;
+        if(username!=null) {
+            for (i = 0; i < username.length(); i++) {
+                if (username.charAt(i) == ' ' || username.charAt(i) == '\t') {
+                    numberSpace++;
+                }
+            }
+        }
+
+        //pt OK
+        if(username!=null && !username.equals("") && numberSpace!=username.length() && !username.contains(" ")){
+            users.setUsername(username.trim());
+        }
+
+        //pt Cancel
+        if(username==null || username.equals("") || numberSpace==username.length() || username.contains(" ")) {
+            JOptionPane.showMessageDialog(frame, "You must insert and validate a username");
+        }
     }
 
     private void insertPersonalPassword(JFrame frame){
 
+        String options[]=new String[]{"OK","Cancel"};
+        JPasswordField passwordField=new JPasswordField();
+        int option=JOptionPane.showOptionDialog(frame,passwordField,"Password", JOptionPane.NO_OPTION,JOptionPane.PLAIN_MESSAGE,null,options,options[1]);
+        boolean choice=false;
+
+        //OK
+        while(option==0){
+            choice=false;
+            char password[]=passwordField.getPassword();
+            int i;
+            for(i=0;i<password.length;i++) {
+                if((password[i]< (int) 'a' || password[i]> (int) 'z')&&(password[i]< (int) 'A' || password[i]> (int) 'Z')&&(password[i]< (int) '0' || password[i]> (int) '9')) {
+                    choice=true;
+                    JOptionPane.showMessageDialog(frame, "Your password can contains only uppercase letters, lowercase letters and digits", "Alert", JOptionPane.WARNING_MESSAGE);
+                    passwordField.setText("");
+                    option=JOptionPane.showOptionDialog(frame,passwordField,"Password", JOptionPane.NO_OPTION,JOptionPane.PLAIN_MESSAGE,null,options,options[1]);
+                    break;
+                }
+            }
+            if(String.valueOf(password).equals("")){
+                choice=true;
+                JOptionPane.showMessageDialog(frame,"Your password can contains only uppercase letters, lowercase letters and digits","Alert", JOptionPane.WARNING_MESSAGE);
+                option=JOptionPane.showOptionDialog(frame,passwordField,"Password", JOptionPane.NO_OPTION,JOptionPane.PLAIN_MESSAGE,null,options,options[1]);
+            }
+            if(choice==false) {
+                JOptionPane.showMessageDialog(frame,"Your password contains valid characters");
+                users.setPassword(String.valueOf(password));
+                break;
+            }
+        }
+
+        //Cancel
+        if(option==1){
+            JOptionPane.showMessageDialog(frame, "You must insert and validate a password", "Alert", JOptionPane.WARNING_MESSAGE);
+        }
     }
 }
--- src/main/java/Persistence/UsersRepository.java
+++ src/main/java/Persistence/UsersRepository.java
@@ -8,23 +8,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UsersRepository {
-    private UsersService usersService=new UsersService();
     public void insertUser(Users users) throws SQLException {
         try (Connection connection = DatabaseConfiguration.getConnection()) {
-
+            UsersService usersService=new UsersService();
             //dupa ce se realizeaza verificarea in insertCredentials() din clasa LoggedIn,
             //in care se testeaza daca userul si parola exista in baza de date
             //acel user si acea parola
             //sunt introduse de 2 ori in baza de date
+            boolean state=false;
             for(Users user:usersService.getUsers()){
-                if(user.getUsername().equals(users.getUsername()) && user.getPassword().equals(users.getPassword()))
+                if(user.getUsername().equals(users.getUsername()) && user.getPassword().equals(users.getPassword())) {
+                    state = true;
+                    break;
+                }
             }
 
-            String insertUser = "INSERT INTO users(`Username`,`Password`) VALUES (?,?)";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertUser);
-            preparedStatement.setString(1,users.getUsername());
-            preparedStatement.setString(2,users.getPassword());
-            preparedStatement.executeUpdate();
+            if(!state) {
+                String insertUser = "INSERT INTO users(`Username`,`Password`) VALUES (?,?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(insertUser);
+                preparedStatement.setString(1, users.getUsername());
+                preparedStatement.setString(2, users.getPassword());
+                preparedStatement.executeUpdate();
+            }
         }
     }
 
